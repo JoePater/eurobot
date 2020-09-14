0889df7648cbe7c4389f782bd95ae2f1aea70df4
Joe Pater <02joepater06@gmail.com>
2020-09-14 19:44:44 +0100

    Begun location file

---
+++ code/py/location.py
@@ -0,0 +1,26 @@
+
+
+#functions for interacting with C++ position code
+
+def position():
+    pass
+
+def bearing():
+    pass
+
+def opponent():
+    pass
+
+def init_team():
+    #check switch, then set team_colour
+    global team_colour
+
+    if buttons.buttonPressed("TEAM_IS_BLUE"):
+        team_colour = "BLUE"
+    else:
+        team_colour = "YELLOW"
+
+    #notify C++ code
+
+def team():
+    return team_colour
