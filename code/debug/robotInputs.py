ee758f8e577365a121487b2700563c156c5196b2
Joe Pater <02joepater06@gmail.com>
2020-11-20 09:56:18 +0000

    Adjusted to take a function queue parameter that is used for all widgets

--- code/debug/robotInputs.py
+++ code/debug/robotInputs.py
@@ -1,29 +1,31 @@
-import tkinter
-top = tkinter.Tk()
+from tkinter import *
+from debuggingGUI import RobotButton, RobotSlider
 
 class RobotInputs:
     '''
-        Button list of parameters that will be passed for buttons
-        the Parameters are Rwindow, Rtext, RFunction
-        Widget list is buttonName, ButtonFunction, lower, upperbound
+    fq: a function queue to use for all buttons and sliders
+
+    button_list: a list of lists, each of which will contain
+    the text and function for a button
+
+    slider_list: a list of lists, each of which will contain
+    the text, function, lower bound and upper bound for a slider
     '''
-    def __init__(self, parent, button_list, slider_list):
+    def __init__(self, parent, fq, button_list, slider_list):
         self.frame = Frame(parent)
         self.button_list = []
         self.slider_list = []
         for i in button_list:
-            r = (RobotButton(self.frame, i[0], i[1], i[2]))
+            r = (RobotButton(self.frame, i[0], i[1], fq))
             r.pack()
             self.button_list.append(r)
         for i in slider_list:
-            r = (RobotSlider(self.frame, i[0], i[1], i[2], i[3], i[4]))
+            r = (RobotSlider(self.frame, i[0], i[1], i[2], i[3], fq))
             r.pack()
-            self.widget_list.append(r)
+            self.slider_list.append(r)
+            
     def pack(self):
         self.frame.pack()
-    def grid(row=0, column):
-        self.frame.grid(column=column, row=row)
         
-        
-        
-top.mainloop()
+    def grid(self,row=0, column=0, rowspan=1):
+        self.frame.grid(column=column, row=row,rowspan=rowspan)
