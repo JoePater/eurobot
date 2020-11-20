67daeb83553a94c3c16128adbe6d8becf7a822f6
Joe Pater <02joepater06@gmail.com>
2020-11-20 09:57:30 +0000

    Error fixes, wrote test

--- code/debug/mainWindow.py
+++ code/debug/mainWindow.py
@@ -1,4 +1,4 @@
-import tkinter
+from tkinter import *
 import debuggingGUI,funcQueue,variableTracker,codeRunner,robotInputs
 
 """
@@ -26,29 +26,47 @@ total_num_coderunners = 8
 class MainWindow:
     def __init__(self,parent,rbuttons,rsliders,varfuncs):
         self.vt = variableTracker.VariableTracker(varfuncs)
-        self.fq = funcQueue.FunctionQueue(self.vt)
+        self.fq = funcQueue.FunctionQueue([self.vt])
 
         self.frame = Frame(parent)
 
         self.fq_widget = funcQueue.FunctionQueueWidget(self.frame,self.fq)
         self.labels = debuggingGUI.RobotLabels(self.frame)
 
-        self.coderunner = codeRunner.CodeRunner(self.frame,self.fq,total_num_coderunners)
+        self.coderunner = codeRunner.CodeRunner(
+            self.frame,self.fq,total_num_coderunners)
 
-        self.inputs = robotInputs.RobotInputs(self.frame,rbuttons,rsliders)
+        self.inputs = robotInputs.RobotInputs(
+            self.frame,self.fq,rbuttons,rsliders)
 
         self.fq_widget.grid(row=0,column=0)
         self.labels.grid(row=1,column=0)
         self.coderunner.grid(row=0,column=1,rowspan=2)
         self.inputs.grid(row=0,column=2,rowspan=2)
 
-    def pack():
+        self.fq.start()
+
+    def pack(self):
         self.frame.pack()
 
-    def grid(row=0,column=0):
+    def grid(self,row=0,column=0):
         self.frame.grid(row=row,column=column)
         
-    def update():
+    def update(self):
         self.fq_widget.update()
-        self.labels.update(self.vt.get_variables)
+        self.labels.update(self.vt.get_variables())
+
+def test():
+    root = Tk()
+    m = MainWindow(root,[],[],{})
+    m.pack()
+    
+    try:
+        while 1:
+            root.update()
+            m.update()
+    except:
+        pass
+    
+    m.fq.kill()
 
